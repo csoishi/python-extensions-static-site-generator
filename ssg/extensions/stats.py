aae56472c56163da87ef12a488c0998b9ccec747
csoishi <25448580+csoishi@users.noreply.github.com>
2022-04-08 15:25:20 -0500

    Completed module

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -7,7 +7,7 @@ total_written = 0
 @hooks.register("start_build")
 def start_build():
     global start_time
-    start_time = time.clock_gettime
+    start_time = time.time()
 
 @hooks.register("written")
 def written():
@@ -16,7 +16,7 @@ def written():
 
 @hooks.event("stats")
 def stats():
-    final_time = time.clock_gettime - start_time
-    average = 0 if total_written ==0 else final_time / total_written
-    report = "Converted: {} . Time: {:.2f} sec . Avg: {:.4f} sec/file"
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
+    report = "Converted: {}  Time: {:.2f} sec  Avg: {:.4f} sec/file"
     print(report.format(total_written, final_time, average))
